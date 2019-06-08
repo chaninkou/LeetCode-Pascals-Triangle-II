d56e2e4598945e3d0f997a605a89501f59021a67
chaninkou <michaelkoulove@yahoo.com>
2019-06-07 23:22:36 -0700

    Pascal's Triangle solution upload.

---
+++ LeetCode-Pascals-Triangle-II/src/pascal/Main.java
@@ -0,0 +1,20 @@
+package pascal;
+
+import java.util.List;
+
+public class Main {
+	public static void main(String[] args) {
+
+		PascalTriangleSolution triangle = new PascalTriangleSolution();
+		
+		int input = 4;
+		
+		System.out.println("Input: " + input);
+		
+		List<Integer> tree = triangle.getRow(input);
+		
+		System.out.println(tree);
+		
+
+	}
+}
---
+++ LeetCode-Pascals-Triangle-II/src/pascal/PascalTriangleSolution.java
@@ -0,0 +1,27 @@
+package pascal;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PascalTriangleSolution {
+    public List<Integer> getRow(int rowIndex) {
+        // rowIndex + 1 cause it's always plus 1 index
+        Integer[] array = new Integer[rowIndex + 1];
+    
+        // To fill complete array with a particular value   
+        Arrays.fill(array, 0);
+    
+        // Cause the first one always start with 1
+        array[0] = 1;
+
+        for (int i = 1; i <= rowIndex; i++)
+        {
+            for (int j = i; j > 0; j--){
+                array[j] = array[j] + array[j - 1];
+            }
+        }
+        
+        // getting the list view of Array 
+        return Arrays.asList(array);
+    }
+}
